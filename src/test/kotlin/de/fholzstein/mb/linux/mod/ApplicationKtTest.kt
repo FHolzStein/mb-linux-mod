0eed6d59befe0dba4dfcbf18ea3ee65266f91b7b
FHolzStein <felix@holzaepfel-stein.de>
2019-01-06 21:27:00 +0100

    make tests throw error to check circleci

--- src/test/kotlin/de/fholzstein/mb/linux/mod/ApplicationKtTest.kt
+++ src/test/kotlin/de/fholzstein/mb/linux/mod/ApplicationKtTest.kt
@@ -1,11 +1,13 @@
 package de.fholzstein.mb.linux.mod
 
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class ApplicationKtTest: StringSpec() {
     init {
         "Main should run successfully" {
             de.fholzstein.mb.linux.mod.main(arrayOf())
+            1 shouldBe  2
         }
     }
 }
