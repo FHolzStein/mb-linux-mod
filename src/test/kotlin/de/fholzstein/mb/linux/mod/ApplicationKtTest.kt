9ae7f308186338444a67e31cae5e0f89b5ddb6b3
FHolzStein <felix@holzaepfel-stein.de>
2019-01-06 21:28:19 +0100

    make test pass again

--- src/test/kotlin/de/fholzstein/mb/linux/mod/ApplicationKtTest.kt
+++ src/test/kotlin/de/fholzstein/mb/linux/mod/ApplicationKtTest.kt
@@ -7,7 +7,6 @@ class ApplicationKtTest: StringSpec() {
     init {
         "Main should run successfully" {
             de.fholzstein.mb.linux.mod.main(arrayOf())
-            1 shouldBe  2
         }
     }
 }
