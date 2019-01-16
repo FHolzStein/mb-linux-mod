515654b641209db0f58248c3c3d00c3201cdfa38
FHolzStein <felix@holzaepfel-stein.de>
2019-01-16 14:19:35 +0100

    add functionality to copy multiple directories and get better results fix test logging fix test structure by using WordSpec instead of StringSpec

--- src/main/kotlin/de/fholzstein/mb/linux/mod/FileService.kt
+++ src/main/kotlin/de/fholzstein/mb/linux/mod/FileService.kt
@@ -1,12 +1,33 @@
 package de.fholzstein.mb.linux.mod
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import java.io.File
+import java.io.IOException
+
+data class CopyDirectoryRequest(val srcDir: File,
+                                val targetDir: File)
+
+data class CopyDirectoryResult(val request: CopyDirectoryRequest,
+                               val success: Boolean,
+                               val errorMessage: String? = null)
 
 class FileService {
-    fun copyDirectory(srcDir: File, targetDir: File) {
-        if (!srcDir.isDirectory || targetDir.isDirectory){
-            return
+    fun copyDirectories(requests: List<CopyDirectoryRequest>): List<CopyDirectoryResult> = runBlocking {
+        requests.map {
+            async {
+                copyDirectory(it)
+            }
+        }.awaitAll()
+    }
+
+    fun copyDirectory(request: CopyDirectoryRequest) : CopyDirectoryResult {
+        val (srcDir, targetDir) = request;
+        if (!srcDir.isDirectory) {
+            return CopyDirectoryResult(request, false, "The source dir ${srcDir.absolutePath} is not a directory")
         }
         srcDir.copyRecursively(targetDir, true)
+        return CopyDirectoryResult(request, true)
     }
 }
--- src/test/kotlin/de/fholzstein/mb/linux/mod/FileServiceTest.kt
+++ src/test/kotlin/de/fholzstein/mb/linux/mod/FileServiceTest.kt
@@ -3,13 +3,15 @@ package de.fholzstein.mb.linux.mod
 import io.kotlintest.Description
 import io.kotlintest.TestResult
 import io.kotlintest.matchers.file.shouldExist
+import io.kotlintest.should
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import io.kotlintest.specs.WordSpec
 import java.io.File
 import java.util.*
 
 
-class FileServiceKtTest : StringSpec() {
+class FileServiceKtTest : WordSpec() {
     val testFiles: MutableList<File> = arrayListOf()
 
     override fun afterTest(description: Description, result: TestResult) {
@@ -26,40 +28,122 @@ class FileServiceKtTest : StringSpec() {
     }
 
     init {
-        "method copyDirectory" {
+        "method copyDirectory" should {
 
 
-            "should copy directory recursively to target" {
+            "copy directory recursively to target" {
                 // given
 
                 val testee = FileService()
                 val testSrc = File(FileServiceKtTest::class.java.classLoader.getResource("FileServiceTest/srcDir").file)
                 val testTarget = createTestDir()
+                val request = CopyDirectoryRequest(testSrc, testTarget)
 
                 // when
-                testee.copyDirectory(testSrc, testTarget)
+                val result = testee.copyDirectory(request)
 
                 // then
                 val testTargetSubFile = File(testTarget.path, "testFile.txt")
 
+                result.request shouldBe  request
+                result.success shouldBe true
                 testTarget.exists() shouldBe true
                 testTarget.listFiles().size shouldBe 1
                 testTargetSubFile.shouldExist()
                 testTargetSubFile.readText() shouldBe "SUCCESS"
-
             }
 
-            "should not copy single files" {
+            "not copy single files" {
                 // given
 
                 val testee = FileService()
                 val testSrc = File(FileServiceKtTest::class.java.classLoader.getResource("FileServiceTest/srcDir/testFile.txt").file)
                 val testTarget = File(UUID.randomUUID().toString())
 
+                val request = CopyDirectoryRequest(testSrc, testTarget)
+
                 // when
-                testee.copyDirectory(testSrc, testTarget)
+                val result = testee.copyDirectory(request)
 
                 // then
+                result.request shouldBe request
+                result.success shouldBe false
+                testTarget.exists() shouldBe false
+
+            }
+        }
+        "method copyDirectories" should {
+            "copy a single directory" {
+                val testee = FileService()
+                val testSrc = File(FileServiceKtTest::class.java.classLoader.getResource("FileServiceTest/srcDir").file)
+                val testTarget = createTestDir()
+
+                val copyDirectoryRequest = CopyDirectoryRequest(testSrc, testTarget)
+
+                // when
+                val result = testee.copyDirectories(listOf(copyDirectoryRequest))
+
+                // then
+                val testTargetSubFile = File(testTarget.path, "testFile.txt")
+
+                result.size shouldBe 1
+
+                val copyDirectoryResult = result[0]
+
+                copyDirectoryResult.success shouldBe true
+                copyDirectoryResult.request shouldBe copyDirectoryRequest
+                copyDirectoryResult.errorMessage shouldBe null
+                testTarget.exists() shouldBe true
+                testTarget.listFiles().size shouldBe 1
+                testTargetSubFile.shouldExist()
+                testTargetSubFile.readText() shouldBe "SUCCESS"
+            }
+            "copy multiple directories" {
+                val testee = FileService()
+                val testSrc = File(FileServiceKtTest::class.java.classLoader.getResource("FileServiceTest/srcDir").file)
+
+                val copyDirectoryRequests = listOf(CopyDirectoryRequest(testSrc, createTestDir()),
+                        CopyDirectoryRequest(testSrc, createTestDir()),
+                        CopyDirectoryRequest(testSrc, createTestDir()),
+                        CopyDirectoryRequest(testSrc, createTestDir()))
+
+                // when
+                val result = testee.copyDirectories(copyDirectoryRequests)
+
+                // then
+                result.size shouldBe 4
+
+                for (copyDirectoryResult in result) {
+                    val copyDirectoryRequest = copyDirectoryResult.request
+                    val testTarget = copyDirectoryRequest.targetDir
+                    val testTargetSubFile = File(testTarget.path, "testFile.txt")
+
+                    copyDirectoryResult.success shouldBe true
+                    copyDirectoryRequest shouldBe copyDirectoryRequest
+                    copyDirectoryResult.errorMessage shouldBe null
+                    testTarget.exists() shouldBe true
+                    testTarget.listFiles().size shouldBe 1
+                    testTargetSubFile.shouldExist()
+                    testTargetSubFile.readText() shouldBe "SUCCESS"
+                }
+            }
+            "not copy single files" {
+                // given
+
+                val testee = FileService()
+                val testSrc = File(FileServiceKtTest::class.java.classLoader.getResource("FileServiceTest/srcDir/testFile.txt").file)
+                val testTarget = File(UUID.randomUUID().toString())
+
+                val request = CopyDirectoryRequest(testSrc, testTarget)
+
+                // when
+                val result = testee.copyDirectories(listOf(request))
+
+                // then
+                result.size shouldBe 1
+                val copyDirectoryResult = result[0]
+                copyDirectoryResult.request shouldBe request
+                copyDirectoryResult.success shouldBe false
                 testTarget.exists() shouldBe false
 
             }
