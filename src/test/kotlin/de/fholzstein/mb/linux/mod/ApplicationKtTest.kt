c006e2a6bf6b47439e4b200dfafeade2228dd7bb
FHolzStein <felix@holzaepfel-stein.de>
2019-01-17 19:05:20 +0100

    make application test run faster by only showing config

--- src/test/kotlin/de/fholzstein/mb/linux/mod/ApplicationKtTest.kt
+++ src/test/kotlin/de/fholzstein/mb/linux/mod/ApplicationKtTest.kt
@@ -5,7 +5,7 @@ import io.kotlintest.specs.StringSpec
 class ApplicationKtTest: StringSpec() {
     init {
         "Main should run successfully" {
-            de.fholzstein.mb.linux.mod.main(arrayOf())
+            de.fholzstein.mb.linux.mod.main(arrayOf("showConfig"))
         }
     }
 }
